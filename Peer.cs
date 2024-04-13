73f7cdcd12ab67146fadff8a3104194bf5f0c674
thamiris <tsadriano.band.com.br>
2024-04-13 06:43:24 -0300

    first commit

---
+++ Peer.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Peer {
+    public TcpClient client;
+    public string address;
+    public List<string> Blockchain = new List<string>();
+
+    public Peer(TcpClient client, string address) {
+        this.client = client;
+        this.address = address;
+    }
+
+    public void Listen() {
+        NetworkStream stream = this.client.GetStream();
+        byte[] receivedBytes = new byte[1024];
+        int byte_count;
+
+        while ((byte_count = stream.Read(receivedBytes, 0, receivedBytes.Length)) > 0) {
+            string data = Encoding.ASCII.GetString(receivedBytes, 0, byte_count);
+            Console.WriteLine("Received: " + data);
+            ProcessData(data);
+        }
+    }
+
+    private void ProcessData(string data) {
+        if (data == "Requesting Blockchain") {
+            SendData(string.Join(",", Blockchain));
+        }
+    }
+
+    public void SendData(string message) {
+        byte[] data = Encoding.ASCII.GetBytes(message);
+        NetworkStream stream = this.client.GetStream();
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Sent: " + message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+class Program {
+    static List<Peer> peers = new List<Peer>();
+
+    static void Main(string[] args) {
+        int port = 8000;  // Porta padrão
+        if (args.Length > 0) {
+            port = int.Parse(args[0]);  // Aceita a porta como argumento
+        }
+
+        StartNode(port);
+    }
+
+    static void StartNode(int port) {
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine("Node started on port " + port);
+
+        while (true) {
+            TcpClient client = listener.AcceptTcpClient();
+            Peer peer = new Peer(client, "localhost:" + port);
+            peers.Add(peer);
+
+            Thread thread = new Thread(new ThreadStart(peer.Listen));
+            thread.Start();
+        }
+    }
+}
+
